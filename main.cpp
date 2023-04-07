caf505956dda5f7e7b4121d3dffe014ca3648d93
11222359_Trinh Minh Hieu <128775728+Trinh-Minh-Hieu-11222359@users.noreply.github.com>
2023-04-07 10:58:29 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
-/******************************************************************************
-Copy đoạn code vào file này
-**Lưu ý không đổi tên file hoặc định dạng file
-**Nộp thành công sẽ có tích xanh ở ngoài repository
-*******************************************************************************/
 #include <stdio.h>
 
-int main()
-{
-    printf("Hello World");
-
+int main() {
+    float distance;
+    float fuel;
+    printf("Input travelled distance: ");
+    scanf("%f", &distance);
+    printf("Input spent fuel: ");
+    scanf("%f", &fuel);
+    printf("Consumed: %.2f litters/100km", fuel / distance * 100);
     return 0;
 }
