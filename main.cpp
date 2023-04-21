24a4bae460da8961b4d3cc24919ece5a8ef452ee
Mai Quang Minh <128773311+Mai-Quang-Minh-11224217@users.noreply.github.com>
2023-04-21 10:01:50 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
-/******************************************************************************
-Copy đoạn code vào file này
-**Lưu ý không đổi tên file hoặc định dạng file
-**Nộp thành công sẽ có tích xanh ở ngoài repository
-*******************************************************************************/
 #include <stdio.h>
 
-int main()
-{
-    printf("Hello World");
+int main() {
+    int distance;
+    float fuel, consumption;
+
+    printf("Enter the total distance traveled (in km): ");
+    scanf("%d", &distance);
+
+    printf("Enter the amount of fuel spent (in liters): ");
+    scanf("%f", &fuel);
+
+    consumption = (fuel / distance) * 100;
+
+    printf("The bike's average consumption is %.2f liters per 100 km.\n", consumption);
 
     return 0;
 }
